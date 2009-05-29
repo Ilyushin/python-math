1e96042a2cabb62a790cf54d4f6f27d54c3d4ba2
Aaron Bycoffe <bycoffe@gmail.com>
2009-05-29 19:06:59 +0000

    Removed Django dependency for parts of module that don't require it.

--- calculate/__init__.py
+++ calculate/__init__.py
@@ -1,17 +1,22 @@
-from competition_rank import competition_rank
 from decile import decile
 from elfi import elfi
-from mean_center import mean_center
 from mean import mean
 from median import median
-from nudge_points import nudge_points
 from ordinal_rank import ordinal_rank
 from per_capita import per_capita
 from per_sqmi import per_sqmi
 from percentage_change import percentage_change
 from percentage import percentage
 from percentile import percentile
-from random_point import random_point
 from standard_deviation import standard_deviation
-from standard_deviation_distance import standard_deviation_distance
-from standard_deviation_ellipses import standard_deviation_ellipses
+
+from sys import modules
+
+# These require Django
+if 'django' in modules:
+    from competition_rank import competition_rank
+    from random_point import random_point
+    from mean_center import mean_center
+    from nudge_points import nudge_points
+    from standard_deviation_distance import standard_deviation_distance
+    from standard_deviation_ellipses import standard_deviation_ellipses
--- calculate/ordinal_rank.py
+++ calculate/ordinal_rank.py
@@ -1,28 +1,25 @@
-from django.db.models.query import QuerySet
+def ordinal_rank(seq, item):
+    """
+    Accepts a sequence and an item and returns the item's
+    ordinal rank as an integer
+    
+    h3. Example usage
+    
+        >> import calculate
+        >> qs = Player.objects.all().order_by("-career_home_runs")
+        >> barry = Player.objects.get(first_name__iexact='Barry', last_name__iexact='Bonds')
+        >> calculate.ordinal_rank(qs, barry)
+        1
+    
+    h3. Documentation
+    
+        * "ordinal rank":http://en.wikipedia.org/wiki/Ranking#Ordinal_ranking_.28.221234.22_ranking.29
 
-def ordinal_rank(queryset, obj):
-	"""
-	Accepts a Django queryset and Django object and
-	returns the object's ordinal rank as an integer.
-	
-	h3. Example usage
-	
-		>> import calculate
-		>> qs = Player.objects.all().order_by("-career_home_runs")
-		>> barry = Player.objects.get(first_name__iexact='Barry', last_name__iexact='Bonds')
-		>> calculate.ordinal_rank(qs, barry)
-		1
-	
-	h3. Dependencies
-	
-		* "django":http://www.djangoproject.com/
-	
-	h3. Documentation
-	
-		* "ordinal rank":http://en.wikipedia.org/wiki/Ranking#Ordinal_ranking_.28.221234.22_ranking.29
-	
-	"""
-	if not isinstance(queryset, QuerySet):
-		raise TypeError('First parameter must be a Django QuerySet. You submitted a %s object' % type(queryset))
-	index = list(queryset).index(obj)
-	return index + 1
+    """
+    try:
+        list_seq = list(seq)
+    except TypeError:
+        raise TypeError('First parameter must be a sequence. You submitted a %s object' % type(seq))
+
+    index = list_seq.index(item)
+    return index + 1
